f69a9dfe3b34fa5b1974643e78f4460aa7b4bbe1
Angel Jackson <angel.jackson@ascte.org>
2025-04-29 13:44:24 -0500

    Login page

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,7 @@
-const username = "admin";
+let username = localStorage.getItem("username")
+if (!username){
+  window.location.href = "/login";
+}
 
 function renderPost(post, isNew = false) {
     const template = document
@@ -33,6 +36,7 @@ async function submitPost() {
 
   window.onload = async () => {
     try {
+      document.getElementById("username").innerText = username;
       const response = await fetch("/api/posts");
       const posts = await response.json();
       posts.forEach((post) => renderPost(post));
